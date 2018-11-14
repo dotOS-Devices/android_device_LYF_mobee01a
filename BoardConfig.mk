ebeae0a368c5f1493f9b8d19f805c3efb0e1fa7c
Mohd Faraz <mohd.faraz.abc@gmail.com>
2018-11-14 23:49:14 +0530

    LS-5015: Switch to Destructive Kernel
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -142,7 +142,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_KERNEL_SEPARATED_DT := true
 LZMA_RAMDISK_TARGETS := recovery
 TARGET_KERNEL_SOURCE := kernel/LYF/mobee01a
-TARGET_KERNEL_CONFIG := odonors_defconfig
+TARGET_KERNEL_CONFIG := destructive_defconfig
 
 # Manifest
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
